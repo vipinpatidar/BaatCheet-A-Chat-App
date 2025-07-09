4d3f39dbad7990f0cf5d16c6f8f78f1e2151697d
Vipin Patidar <99580663+vipinpatidar@users.noreply.github.com>
2025-07-09 12:21:54 +0530

    fix deployment issue with rate limits

--- Server/app.js
+++ Server/app.js
@@ -40,7 +40,7 @@ const io = new Server(httpServer, {
     ],
     credentials: true,
   },
-});
+})
 
 app.set("io", io); // using set method to mount the `io` instance on the app to avoid usage of `global`
 
